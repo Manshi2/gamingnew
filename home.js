b1dd4f1129813d2fd3dc22ee9d2bbe9a72867143
Manshi2 <manshigusain98@gmail.com>
2024-06-11 21:21:35 +0530

    update

--- home.js
+++ home.js
@@ -306,9 +306,6 @@ process.on('SIGINT', function() {
     process.exit(0);
   });
 });
-// Start the server
-// app.listen(process.env.PORT || 5000, function() {
-//   console.log("Server is running on port 5000");
-// });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
